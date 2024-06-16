2fa8b4022d875250343a028b4d17973df09afb7e
nachoNota <nachonota13@gmail.com>
2024-06-16 11:50:33 -0300

    API de Marvel añadida para poder trabajar con sus personajes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LlamadaAPI;
+
+var ListaNombresDescripciones = await APICLIENT.GetCharactersAsync();
+
+foreach (var Personaje in ListaNombresDescripciones.data.Resultados)
+{
+    if(Personaje.Descripcion  != "")
+    {
+        Console.WriteLine($"Name: {Personaje.Nombre}, Description: {Personaje.Descripcion}\n");
+    }
+}
+
