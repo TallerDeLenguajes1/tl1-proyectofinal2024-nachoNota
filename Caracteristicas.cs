66bdb3f8b50d44e285f3eaeb3c76062a2fa61978
nachoNota <nachonota13@gmail.com>
2024-06-22 19:06:21 -0300

    Agrego a las caracteristicas una serie de ataques especiales

--- Caracteristicas.cs
+++ Caracteristicas.cs
@@ -6,11 +6,13 @@ public class Caracteristicas
     private int daño { get; set; }
     private int defensa { get; set; }
     private int mana { get; set; }
+    private List<Especial> especiales { get; set; }
 
     public int Salud { get => salud; set => salud = value; }
     public int Daño { get => daño; set => daño = value; }
     public int Defensa { get => defensa; set => defensa = value; }
     public int Mana { get => mana; set => mana = value; }
+    public List<Especial> Especiales { get => especiales; set => especiales = value; }
 
     public Caracteristicas()
     {
@@ -18,6 +20,12 @@ public class Caracteristicas
         Mana = 100;
         Daño = GenerarAleatorio();
         Defensa = GenerarAleatorio();
+        Especiales = new List<Especial>
+        {
+            new Especial("Curacion", "+25 de Salud (-50 de Mana)", 0, 0, 25),
+            new Especial("Proteccion Divina", "+25 de Defensa (-50 de Mana)", 0, 25, 0),
+            new Especial("Corte Profundo", "25 de daño (-50 de Mana)", 25, 0, 0)
+        };
     }
 
     Random rdm = new Random();
@@ -27,5 +35,30 @@ public class Caracteristicas
         return rdm.Next(10, 20);
     }
 
+}
 
+public class Especial
+{
+    private string nombre { get; set; }
+    private string descripcion {  get; set; }
+    private int daño {  get; set; }
+    private int defensa { get; set; }
+    private int salud { get; set; }
+    private int costoMana { get; set; }
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Descripcion { get => descripcion; set => descripcion = value; }
+    public int Daño { get => daño; set => daño = value; }
+    public int Defensa { get => defensa; set => defensa = value; }
+    public int Salud { get => salud; set => salud = value; }
+    public int CostoMana { get => costoMana; set => costoMana = value; }
+
+    public Especial(string Nombre, string Descripcion, int Daño, int Defensa, int Salud)
+    {
+        this.Nombre = Nombre;
+        this.Descripcion = Descripcion;
+        this.Daño = Daño;
+        this.Defensa = Defensa;
+        CostoMana = 50;
+    }
 }
