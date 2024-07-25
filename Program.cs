d3c7b8c293d2d1946f699d8d5879b75d28bbe98d
nachoNota <nachonota13@gmail.com>
2024-07-25 13:27:29 -0300

    Trabajo en la jugabilidad del torneo

--- Program.cs
+++ Program.cs
@@ -1,111 +1,164 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using MovimientosCombate;
+using VentanaCombate;
+using MiProyecto.FabricaDePersonajes;
 
 Console.Clear();
-Random rdm = new Random(); //Para seleccionar personajes al azar
-var Texto = new FuncionesTexto(); //Para las funciones de texto
 
-var ListaNombresDescripciones = await APICLIENT.GetCharactersAsync();
-//Creo personajes con sus datos y caracteristicas y hago lista que los contenga
+var Texto = new FuncionesTexto();
+var Valid = new ValidarOpciones();
+var Combate = new Combate();
 var Fabrica = new FabricaDePersonajes();
-var ListaDePersonajes = new List<Personaje>();
-foreach (var Personaje in ListaNombresDescripciones.data.Resultados)
+var ListaNombresDescripciones = await APICLIENT.GetCharactersAsync();
+
+int OpcionMenu = 0;
+do
 {
-    if(Personaje.Descripcion != "")
-    {        
-        var PersonajeCreado = Fabrica.CrearPersonaje(new Datos(Personaje.Nombre, Personaje.Descripcion));
-        ListaDePersonajes.Add(PersonajeCreado);
-    } 
-}
+    //Muestro el menu con las opciones a elegir 
+    Texto.Logo();
+    Texto.Menu();
 
+    Console.WriteLine();
 
-//Muestro el menu con las opciones a elegir 
-int opcion_menu = 0;
-bool opcion_valida = false;
-Texto.Logo();
-Texto.Menu();
+    Console.Write(Texto.CentrarLinea("Elija una de las opciones: "));
+    OpcionMenu = Valid.ValidarOpcion(4);
 
-while (!opcion_valida)
-{
-    Console.WriteLine("Elija una de las opciones: ");
-    string input = Console.ReadLine();
-
-    if (int.TryParse(input, out opcion_menu) && (opcion_menu >= 1 && opcion_menu <= 3))
+    Console.Clear();
+    if(OpcionMenu != 4)
     {
-        opcion_valida = true;
+        //Creo personajes con sus datos y caracteristicas y hago lista que los contenga
+        var ListaDePersonajes = Fabrica.CrearListaPersonajes(ListaNombresDescripciones);
+        var MovimientosPorClave = Combate.CrearClavesMovimientos();
+        switch (OpcionMenu)
+        {
+            case 1:
+                //Muestro los personajes para que el usuario pueda elegir el suyo
+                Texto.MostrarPersonajes(ListaDePersonajes);
+                Console.Write("Con qué personaje quiere pelear? (ingresar su numero): ");
+                int opcionPersonaje = Valid.ValidarOpcion(ListaDePersonajes.Count);
+
+                Console.Clear();
+
+                Personaje PersonajeElegido = Fabrica.PersonajeElegido(ListaDePersonajes, opcionPersonaje);
+                Console.WriteLine($"\nMUY BIEN! El personaje elegido es: \n\n");
+                Texto.MostrarPersonaje(PersonajeElegido);
+                await Task.Delay(4000);
+
+                Console.Clear();
+
+                Console.WriteLine("En cuartos de final tendras que enfrentarte a...\n");
+                await Task.Delay(2000);
+
+                Personaje Oponente = Fabrica.PersonajeAlAzar(ListaDePersonajes);
+                Texto.MostrarPersonaje(Oponente);
+                await Task.Delay(4000);
+
+                Console.Clear();
+
+                //await Combate.NuevoCombate(PersonajeElegido, Oponente, MovimientosPorClave);
+
+                if(Combate.EsGanador(PersonajeElegido))
+                {
+                    await Texto.EscribirTextoAsync("Felicidades, lograste avanzar a las semifinales de este torneo, pero no cantes victoria, las cosas de ahora en adelante se pondrán mas complicadas...\n\n");
+                    await Task.Delay(1000);
+                    PersonajeElegido.Caracteristicas.SubirEstadisticasJugador();
+
+                    Console.WriteLine("Para estas instancias, tu rival será...\n");
+                    await Task.Delay(2000);
+                    
+                    Oponente = Fabrica.PersonajeAlAzar(ListaDePersonajes);
+                    Oponente.Caracteristicas.SubirNivelOponente();
+                    
+                    Texto.MostrarPersonaje(Oponente);
+                    
+                    await Task.Delay(4000);
+                    
+                    Console.Clear();
+
+                  //  await Combate.NuevoCombate(PersonajeElegido, Oponente, MovimientosPorClave);
+
+                        PersonajeElegido.Caracteristicas.Salud = 0;
+                    if (Combate.EsGanador(PersonajeElegido))
+                    {
+                        await Texto.EscribirTextoAsync("¿Que? ¿En serio llegaste a la final? La verdad que no me esperaba que llegues tan lejos, voy a tener que ponerte las cosas más complicadas...\n\n");
+                        await Task.Delay(1000);
+                        PersonajeElegido.Caracteristicas.SubirEstadisticasJugador();
+
+                        Console.WriteLine("Para estas instancias, tu rival será...\n");
+                        await Task.Delay(2000);
+
+                        Oponente = Fabrica.PersonajeAlAzar(ListaDePersonajes);
+                        for(int i = 0; i < 2; i++)
+                        {
+                            Oponente.Caracteristicas.SubirNivelOponente();
+                        }
+                        Texto.MostrarPersonaje(Oponente);
+
+                        await Task.Delay(4000);
+
+                        Console.Clear();
+
+                        //await Combate.NuevoCombate(PersonajeElegido, Oponente, MovimientosPorClave);
+                        if (Combate.EsGanador(PersonajeElegido))
+                        {
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\n");
+                            await Texto.EscribirTextoAsync("JAAAAAAAAAAAAAAAAAAAAAAAA JA JA JA JA JA JA\n");
+                            await Task.Delay(1000);
+                            
+                            await Texto.EscribirTextoAsync("ah, perdon. Donde estan mis modales...\n");
+                            await Task.Delay(1000);
+                           
+                            await Texto.EscribirTextoAsync("Te felicito por haber llegado tan lejos, pocos héroes logran estar donde estás parado ahora mismo, lástima que tuviste que verte las caras con mi peleador mas fuerte.\n");
+                            await Task.Delay(1000);
+                           
+                            await Texto.EscribirTextoAsync("ah, casi se me olvida, he agregado el nombre de mi campeón a la lista de historicos, para que cada vez que entres puedas ver el dia exacto en el que perdiste");
+                            await Task.Delay(1000);
+                            Console.Write(" :D");
+  
+                            Combate.GuardarCampeon("CampeonesHistoricos.json", Oponente);
+
+                            await Task.Delay(1000);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\n");
+                        await Texto.EscribirTextoAsync("Bueno... Sabia que no llegarías mas lejos que esto.\n");
+                        await Task.Delay(1000);
+                        await Texto.EscribirTextoAsync("Al fin y al cabo, pocos fueron los que lograron enfrentarse a mi campeón mas fuerte\n");
+                        await Task.Delay(1000);
+                        await Texto.EscribirTextoAsync("Pero bueno, ¿quien te dice que en el proximo intento no llegarás más lejos? Acá estaré esperando...\n");
+                        await Task.Delay(1500);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n\n");
+                    await Texto.EscribirTextoAsync("Rival complicado eh?\n");
+                    await Task.Delay(1000);
+                    await Texto.EscribirTextoAsync("No te preocupers, siempres puedes arrancar el torneo desde cero, solo trata de no perder la paciencia!\n");
+                    await Task.Delay(1000);
+                    await Texto.EscribirTextoAsync("Te deseo suerte para la proxima!!");
+                    await Task.Delay(1000);
+                }
+
+                break;
+            case 2:
+                break;
+        }
     }
     else
     {
-        Console.WriteLine("Por favor, ingrese un número válido entre 1 y 3.");
+        Console.WriteLine("Esperamos que vuelvas a jugar pronto, suerte!");
+        await Task.Delay(3000);
     }
-}
 
-Console.Clear();
+    Console.Clear();
 
-//Muestro los personajes para que el usuario pueda elegir el suyo
-int opcion_personaje = 0;
-opcion_valida = false;
-Texto.MostrarPersonajes(ListaDePersonajes);
-
-while (!opcion_valida)
-{
-    Console.WriteLine("Con qué personaje quiere pelear? (ingresar su numero): ");
-    string input = Console.ReadLine();
-
-    if (int.TryParse(input, out opcion_personaje) && (opcion_personaje >= 1 && opcion_personaje <= 8))
-    {
-        opcion_valida = true;
-    }
-    else
-    {
-        Console.WriteLine("Por favor, ingrese un número válido entre 1 y 8.");
-    }
-}
-
-Console.Clear();
-
-Personaje PersonajeElegido = ListaDePersonajes[opcion_personaje - 1];
-Console.WriteLine($"\nMUY BIEN! El personaje elegido es: ");
-Texto.MostrarPersonaje(PersonajeElegido, opcion_personaje);
-ListaDePersonajes.Remove(PersonajeElegido);
-
-switch (opcion_menu)
-{
-    case 1: 
-            Console.WriteLine("Estamos armando el torneo, aguarde unos segundos.");
-            await Task.Delay(10000);
-
-            Console.Clear();
-
-            Console.WriteLine("En cuartos de final tendras que enfrentarte a...");
-            await Task.Delay(4000);
-            int random = rdm.Next(0, 6);
-            Personaje PersonajeACombatir = ListaDePersonajes[random];
-            Texto.MostrarPersonaje(PersonajeACombatir, random+1);
-            ListaDePersonajes.Remove(PersonajeACombatir);
-            await Task.Delay(5000);
-
-            Console.Clear();
-
-            Console.WriteLine("\nComienza el combate!!!");
-
-            await Task.Delay(2000);
-
-            Console.Clear();
-        
-            while (PersonajeElegido.CaracteristicasPersonaje.Salud > 0 && PersonajeACombatir.CaracteristicasPersonaje.Salud > 0)
-            {
-                
-            }
-        
-        break;
-    case 2: 
-            await Task.Delay(5000);
-            
-        break;
-    case 3: Console.WriteLine("\nEsperamos que vuelvas a jugar pronto, suerte!");
-            await Task.Delay(3000);
-        break;
-}
+} while (OpcionMenu != 4);
