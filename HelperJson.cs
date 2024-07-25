ec41f2e5ad0e7847f7ac15c51fe79c587ac5acde
nachoNota <nachonota13@gmail.com>
2024-07-25 13:46:12 -0300

    Agrego el helper de json para la creacion y apertura de archivos

---
+++ HelperJson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class HelperJson
+{
+    public void GuardarArchivo(string nombreArchivo, string datos)
+    {
+        using (FileStream archivo = new FileStream(nombreArchivo, FileMode.Create))
+        {
+            using (StreamWriter sw = new StreamWriter(archivo))
+            {
+                sw.WriteLine(datos);
+                sw.Close();
+            }
+        }
+    }
+
+    public string AbrirArchivo(string nombreArchivo)
+    {
+        string linea;
+        using (FileStream archivo = new FileStream(nombreArchivo, FileMode.Open))
+        {
+            using (StreamReader sr = new StreamReader(archivo))
+            {
+                linea = sr.ReadToEnd();
+                archivo.Close();
+            }
+        }
+        return linea;
+    }
+}
--- ValidarOpciones.cs
+++ ValidarOpciones.cs
@@ -1,4 +1,6 @@
 using System;
+using MovimientosCombate;
+
 
 public class ValidarOpciones
 {
